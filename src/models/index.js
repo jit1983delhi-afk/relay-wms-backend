37cab9936cd40ad9408f1da9dd02f528946bf171
jit1983delhi-afk <jit1983delhi@gmail.com>
2025-11-01 00:10:39 +0530

    Add Admin model to exports in index.js

--- src/models/index.js
+++ src/models/index.js
@@ -5,6 +5,7 @@ const Product = require('./product');
 const Stock = require('./stock');
 const Inward = require('./inward');
 const Outward = require('./outward');
+const Admin = require("./admin");
 
 Warehouse.hasMany(Stock, { foreignKey: 'warehouse_id' });
 Product.hasMany(Stock, { foreignKey: 'product_id' });
@@ -12,3 +13,5 @@ Stock.belongsTo(Warehouse, { foreignKey: 'warehouse_id' });
 Stock.belongsTo(Product, { foreignKey: 'product_id' });
 
 module.exports = { sequelize, User, Warehouse, Product, Stock, Inward, Outward };
+module.exports = { sequelize, User, Warehouse, Product, Stock, Inward, Outward, Admin };
+
